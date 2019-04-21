31daa31a592a814f2b93a054593c263f0505ca04
Martin Rovere <martinr@qb9.net>
2019-04-21 02:18:57 -0300

    un goblin disparando una flecha

--- game/first.gd
+++ game/first.gd
@@ -6,5 +6,17 @@ var player
 func _ready():
 	player = player_scene.instance()
 	add_child(player)
-	player.position.x = 200
+	player.position.x = 100
 	player.position.y = 200
+	
+	player.get_node("AnimatedSprite").play("walk")
+	
+	
+func _process(delta):
+	if player.position.x < 400:
+		player.position += Vector2(150,0) * delta
+	else:
+		if player.position.x < 403:
+			player.position.x = 403
+			player.get_node("AnimatedSprite").play("shoot")
+	
