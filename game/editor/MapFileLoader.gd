25c21576d208cdad60a2281626a6c7fb9d8e2843
Martin Rovere <martinr@qb9.net>
2019-04-30 00:22:44 -0300

    cargo y escribo sobre archivo config

---
+++ game/editor/MapFileLoader.gd
@@ -0,0 +1,36 @@
+extends Node
+
+const SAVE_PATH = "res://editor/map.cfg"
+
+signal map_ready
+
+var _map_file = ConfigFile.new()
+
+var _initData = {
+	"grid_size": {
+		"x": 12,
+		"y": 12
+		}
+	}
+	
+func _ready():
+	var err = _map_file.load(SAVE_PATH)
+	if err == OK:
+		if not _map_file.has_section_key("grid_size", "x"):
+        	_map_file.set_value("grid_size", "x", _initData["grid_size"]["x"])
+			
+		if not _map_file.has_section_key("grid_size", "y"):
+        	_map_file.set_value("grid_size", "y", _initData["grid_size"]["y"])
+			
+		_map_file.save(SAVE_PATH)
+	
+	
+func _on_tile_set_room(current_grid_coord, level_name, north_door, east_door, west_door, south_door):
+	print(current_grid_coord, level_name, north_door, east_door, west_door, south_door)
+	var section = "grid:" + str(current_grid_coord.x) + "," + str(current_grid_coord.y)
+	_map_file.set_value(section, "name", level_name)
+	_map_file.set_value(section, "N", north_door)
+	_map_file.set_value(section, "E", east_door)
+	_map_file.set_value(section, "W", west_door)
+	_map_file.set_value(section, "S", south_door)
+	_map_file.save(SAVE_PATH)
--- game/editor/TileMap.gd
+++ game/editor/TileMap.gd
@@ -3,7 +3,23 @@ extends TileMap
 
 signal custom
 
+var WIDTH
+var HEIGHT
+var current_grid_coord
+
+
 func _ready():
+	
+	WIDTH = MapFileLoader._map_file.get_value("grid_size", "x")
+	HEIGHT = MapFileLoader._map_file.get_value("grid_size", "y")
+	
+	#Inicializar todos los tiles que tengan data
+	
+	$Properties/save_button.connect("button_down", self, "_on_save_button")
+	
+	#CARGAR LA INFO DEL 0,0
+	_load_panel_info(Vector2(0,0))
+	
 	#TUTORIAL SOBRE COMO GUARDAR INFO EN FILESYSTEM
 	#https://www.youtube.com/watch?v=IrUhyf-g5hU
 	#TUTORIAL SOBRE COMO RECUPERAR DATA DEL FILESYSTEM
@@ -11,30 +27,59 @@ func _ready():
 	
 	#DISEÑANDO UN EDITOR DE TILES CUSTOM
 	#https://steemit.com/utopian-io/@sp33dy/tutorial-godot-engine-v3-gdscript-custom-tilemap
-	
 	#CONTINUACION
 	#https://steemit.com/cn/@sp33dy/tutorial-godot-engine-v3-gdscript-custom-tilemap-scrolling
-	pass
-
 
 
 func _input(event):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
 		var mouse_position= get_viewport().get_mouse_position()
-		var map_coord= world_to_map(mouse_position)
-		var clicked_grid_coord = map_to_world(map_coord)
-		$Selector.set_position(clicked_grid_coord)
-		var cell= get_cell(map_coord.x, map_coord.y)
-		if cell != -1 and cell < tile_set.get_tiles_ids().size()-1:
-			cell+= 1
-		else:
-			cell= 0
-			
-			
-		print(map_coord, cell)
-		#SI ESTA EN 0, LE PREGUNTO AL EDITOR, QUE VALORES VAN A IR ACA?
-		#ESO LO HAGO CON UN PANEL CON VARIOS TEXTFIELDS PARA LLENAR DATA
-		#QUE LEVEL? COMO SE COMUNICA CON SUS VECINOS?
+		var old_current_grid= current_grid_coord
+		current_grid_coord= world_to_map(mouse_position)
 		
-		set_cellv(map_coord, cell)
-			
+		if (current_grid_coord.x > WIDTH or current_grid_coord.y > HEIGHT):
+			current_grid_coord= old_current_grid
+			return
+		
+		var cursor_pos = map_to_world(current_grid_coord)
+		$Selector.set_position(cursor_pos)
+		
+		print("_input", current_grid_coord)
+		
+		_load_panel_info(current_grid_coord)
+		
+		#var cell= get_cell(current_grid_coord.x, current_grid_coord.y)
+		#if cell != -1 and cell < tile_set.get_tiles_ids().size()-1:
+		#	cell+= 1
+		#else:
+		#	cell= 0
+
+
+func _load_panel_info(coord):
+	print("_load_panel_info: ", coord)
+	var section = "grid:" + str(coord.x) + "," + str(coord.y)
+
+	if (not MapFileLoader._map_file.has_section_key(section, "name")):
+		$Properties/Level_Name.set_text( "" )
+		$Properties/North_Door.set_text( "" )
+		$Properties/East_Door.set_text( "" )
+		$Properties/West_Door.set_text( "" )
+		$Properties/South_Door.set_text( "" )
+		return
+	
+	$Properties/Level_Name.set_text( MapFileLoader._map_file.get_value(section, "name") )
+	$Properties/North_Door.set_text( str( MapFileLoader._map_file.get_value(section, "N") ) )
+	$Properties/East_Door.set_text( str( MapFileLoader._map_file.get_value(section, "E") ) )
+	$Properties/West_Door.set_text( str( MapFileLoader._map_file.get_value(section, "W") ) )
+	$Properties/South_Door.set_text( str( MapFileLoader._map_file.get_value(section, "S") ) )
+	
+
+func _on_save_button():
+	var level_name = $Properties/Level_Name.get_text()
+	var north_door = $Properties/North_Door.get_text()
+	var east_door = $Properties/East_Door.get_text()
+	var west_door =$Properties/West_Door.get_text()
+	var south_door = $Properties/South_Door.get_text()
+	
+	print("saving on: ", current_grid_coord)
+	MapFileLoader._on_tile_set_room(current_grid_coord, level_name, north_door, east_door, west_door, south_door)
