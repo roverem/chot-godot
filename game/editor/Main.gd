7486c5084391ddd5a38013a848c5c807d7882716
Martin Rovere <martinr@qb9.net>
2019-05-04 02:04:24 -0300

    progreso portal editor

--- game/editor/Main.gd
+++ game/editor/Main.gd
@@ -1,9 +1,19 @@
-tool
 extends Node2D
 
+var tile_map
 
 func _ready():
-	$TileMap.connect("openPortalEditor", self, "_on_portal_edit")
+	$TileMap.connect("openPortalEditor", self, "_on_portal_editor_open")
+	$PortalEditor.connect("closePortalEditor", self, "_on_portal_editor_close")
+	
+	#MIGRAR EL FILE LOADING ACÀ
 
-func _on_portal_edit(sceneRes):
+func _on_portal_editor_open(sceneRes):
+	tile_map = $TileMap
+	remove_child(tile_map)
+	
 	$PortalEditor._loadScene(sceneRes)
+	
+func _on_portal_editor_close():
+	$PortalEditor._removeScene()
+	add_child(tile_map)
--- game/editor/PortalEditor.gd
+++ game/editor/PortalEditor.gd
@@ -1,9 +1,32 @@
 extends Node2D
 
+signal closePortalEditor
+
+var currentScene
 
 func _ready():
-	pass # Replace with function body.
+	$close_button.connect("button_down", self, "_on_close_button")
+	$close_button.visible = false
+	
+func _on_close_button():
+	emit_signal("closePortalEditor")
+
 
 func _loadScene(sceneRes):
-	var scene = sceneRes.instance()
-	add_child(scene)
+	#CARGAR LOS NODOS QUE YA ESTEN LISTOS EN ESTA ESCENA
+	
+	#PREPARAR LOS LISTENERS PARA DIBUJAR PORTALES
+	
+	$close_button.visible = true
+	currentScene = sceneRes.instance()
+	add_child(currentScene)
+	
+func _removeScene():
+	#APAGAR LOS LISTENERS PARA DIBUJAR PORTALES
+	
+	$close_button.visible = true
+	remove_child(currentScene)
+	
+	
+func _draw():
+	pass
